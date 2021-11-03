eda60ce210121c2628e85a04cfb45869d9ac70cc
hverdonk <hverdonk@berkeley.edu>
2021-11-03 16:40:28 -0400

    CleanData __init__ now calls pandas.DataFrame __init__

--- CleanData.py
+++ CleanData.py
@@ -11,7 +11,8 @@ class CleanData(pd.DataFrame):
         FILENAMES: a list of files containing the genes we want to analyze.
         TODO: improve description
         '''
-        self.df = self.__extractdata__(filenames)
+        pd.DataFrame.__init__(self)
+        self.dataframe = __extractdata__(filenames)
 
 
     def get_overexpressed(self):
@@ -26,47 +27,65 @@ class CleanData(pd.DataFrame):
         '''
         return self.df[self.df['expressionChange']=='underexpressed']
         
-    def __extractdata__(files):
-        DEgenes = pd.DataFrame(columns=['geneNames', "baseMean", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj"])
-        for file in files:
-            try:
-                frame = pd.read_csv(file, sep=' ', header=0, index_col=0)
-                # The genes with NA are the ones DESeq2 has filtered out. From DESeq2 manual: “The results function of the DESeq2 package 
-                # performs independent filtering by default using the mean of normalized counts as a filter statistic. A threshold on the 
-                # filter statistic is found which optimizes the number of adjusted p values lower than a [specified] significance level”.
+def __extractdata__(files):
+    '''
+    Given a list of files, extract all genes and their respective data. Filter genes 
+    '''
+    DEgenes = pd.DataFrame(columns=['geneNames', "baseMean", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj"])
+    for file in files:
+        try:
+            frame = pd.read_csv(file, sep=' ', header=0, index_col=0)
+            # Genes with low count numbers and outlier samples have a padj of "NA"
 
-                # I used the default False Discovery Rate (FDR) cutoff of 0.1 as the specified significance level for thresholding
-                # adjusted p-values in the independent filtering step.
-                frame.dropna(subset=['padj'], inplace=True)
-                frame.reset_index(inplace=True)
-                frame = frame.rename(columns = {'index':'geneNames'})
+            # The genes with NA are the ones DESeq2 has filtered out. From DESeq2 manual: “The results function of the DESeq2 package 
+            # performs independent filtering by default using the mean of normalized counts as a filter statistic. A threshold on the 
+            # filter statistic is found which optimizes the number of adjusted p values lower than a [specified] significance level”.
 
-                # TODO: look up the default p_adj threshold for DESeq2
+            # I used the default False Discovery Rate (FDR) cutoff of 0.1 as the specified significance level for thresholding
+            # adjusted p-values in the independent filtering step.
+            frame.dropna(subset=['padj'], inplace=True)
+            frame.reset_index(inplace=True)
+            frame = frame.rename(columns = {'index':'geneNames'})
 
-                # find overexpressed and underexpressed genes for each comparison
-                # sort by lfc, keep only stuff above upper lfc_thr=-1.0 and below lower lfc_thr=1.0
-                frame = frame[(frame['log2FoldChange'] < -1.0) | (frame['log2FoldChange'] > 1.0)]
-                # sort by padj (lowest to highest), keep only stuff above pv_thr=0.05
-                frame = frame[frame['padj'] < 0.05]
-                frame.reset_index(inplace=True)
-                # note which genes are over- and underexpressed
-                numrows = frame.shape[0]
-                emptycol = ['blank']*numrows
-                frame['expressionChange'] = emptycol
-                for row in range(numrows):
-                    if frame.at[row, 'log2FoldChange'] < 0:
-                        frame.at[row, 'expressionChange'] = 'underexpressed'
-                    elif frame.at[row, 'log2FoldChange'] > 0:
-                        frame.at[row, 'expressionChange'] = 'overexpressed'
+            # TODO: look up the default p_adj threshold for DESeq2
 
-                DEgenes = DEgenes.append(frame)
+            # find overexpressed and underexpressed genes for each comparison
+            # sort by lfc, keep only stuff above upper lfc_thr=-1.0 and below lower lfc_thr=1.0
+            frame = frame[(frame['log2FoldChange'] < -1.0) | (frame['log2FoldChange'] > 1.0)]
+            # sort by padj (lowest to highest), keep only stuff above pv_thr=0.05
+            frame = frame[frame['padj'] < 0.05]
+            frame.reset_index(inplace=True)
+            # note which genes are over- and underexpressed
+            numrows = frame.shape[0]
+            emptycol = ['blank']*numrows
+            frame['expressionChange'] = emptycol
+            for row in range(numrows):
+                if frame.at[row, 'log2FoldChange'] < 0:
+                    frame.at[row, 'expressionChange'] = 'underexpressed'
+                elif frame.at[row, 'log2FoldChange'] > 0:
+                    frame.at[row, 'expressionChange'] = 'overexpressed'
 
-            except Exception as e:
-                print(Exception, e)
-                print(file)
+            DEgenes = DEgenes.append(frame)
 
-            # if a gene is an outlier at multiple time points, only keep the first entry in the dataframe
-            # TODO: should I keep duplicate with most significant padj value regardless of timepoint? Or just 
-            # stick to the first timepoint for consistency?
-            DEgenes.drop_duplicates(subset='geneNames', inplace=True)
-            return DEgenes
+        except Exception as e:
+            print(Exception, e)
+            print(file)
+
+    # if a gene is an outlier at multiple time points, only keep the first entry in the dataframe
+    # TODO: should I keep duplicate with most significant padj value regardless of timepoint? Or just 
+    # stick to the first timepoint for consistency?
+
+    print(DEgenes.head())
+    # check if duplicates are overexpressed at one timepoint and underexpressed at another
+    # print(DEgenes[DEgenes.duplicated(subset='geneNames', keep=False)])
+
+    DEgenes.drop_duplicates(subset='geneNames', inplace=True)
+    return DEgenes
+
+
+
+
+filepaths = ['/home/hannah/Dropbox/Temple/kulathinal-rotation/DE_results/FCGXXM-vs-FCGXXF-105-diffexp']
+
+XXMvsXXF_DEGs = CleanData(filepaths)
+print(XXMvsXXF_DEGs.head())
