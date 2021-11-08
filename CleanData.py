10870c60e88db794de9f2daef06576249a6e0234
hverdonk <hverdonk@berkeley.edu>
2021-11-08 14:34:51 -0500

    STRING db querying implemented, updates to CleanData

--- CleanData.py
+++ CleanData.py
@@ -1,94 +1,75 @@
 import pandas as pd
 
-# pd.DataFrame
+# pd.DataDEgenes
 class CleanData():
     '''
-    A special type of Pandas dataframe.
+    A special type of Pandas dataDEgenes.
 
     NOTE: for timepoint-based analyses, make separate CleanData objects for each timepoint's comparison
     '''
-    def __init__(self, filenames):
+    def __init__(self, filename='', dataframe=None):
         '''
-        FILENAMES: a list of files containing the genes we want to analyze.
+        FILENAME: a file containing the genes we want to analyze.
         TODO: improve description
         '''
-        self.df = __extractdata__(filenames)
+        if dataframe:
+            self.df=dataframe
+        else:
+            self.df = __extractdata__(filename)
+
+
+    def genes(self):
+        '''
+        Returns an ndarray of the gene names in self.df
+        '''
+        return self.df['geneNames']
+
+    def genes_string(self):
+        '''
+        Returns a string of comma-separated gene names in self.df
+        Intended for easy search using the STRING database app in cytoscape
+        '''
+        return ','.join(self.genes())
 
 
     def get_overexpressed(self):
         '''
-        Returns a copy of self.df containing only overexpressed genes
+        Returns a new CleanData object where object.df is a copy of self.df containing only overexpressed genes
         '''
-        return self.df[self.df['expressionChange']=='overexpressed']
+        return CleanData(self.df[self.df['expressionChange']=='overexpressed'])
 
     def get_underexpressed(self):
         '''
-        Returns a copy of self.df containing only underexpressed genes
+        Returns a new CleanData object where object.df is a copy of self.df containing only underexpressed genes
         '''
-        return self.df[self.df['expressionChange']=='underexpressed']
+        return CleanData(self.df[self.df['expressionChange']=='underexpressed'])
         
-def __extractdata__(files):
+def __extractdata__(file, pthresh=0.05, logthresh=1.0):
     '''
-    Given a list of files, extract all genes and their respective data. Filter genes 
+    Given a file of differentially expressed genes (yielded by DESeq2), extract all genes and their respective data. Filter genes 
+    by adjusted p-value (padj) and log2 fold change, keeping only those above the appropriate threshold (pthresh and logthresh, respectively)
     '''
-    DEgenes = pd.DataFrame(columns=['geneNames', "baseMean", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj"])
-    for file in files:
-        try:
-            frame = pd.read_csv(file, sep=' ', header=0, index_col=0)
-            # Genes with low count numbers and outlier samples are prefiltered by DESeq2 and have a padj of "NA"
-            frame.reset_index(inplace=True)
-            frame = frame.rename(columns = {'index':'geneNames'})
-
-            # TODO: look up the default p_adj threshold for DESeq2
-
-            # find differentially expressed genes for each comparison
-            # sort by lfc, keep only stuff above upper lfc_thr=-1.0 and below lower lfc_thr=1.0
-            frame = frame[(frame['log2FoldChange'] < -1.0) | (frame['log2FoldChange'] > 1.0)]
-            # sort by padj (lowest to highest), keep only stuff above pv_thr=0.05
-            frame = frame[frame['padj'] < 0.05]
-            frame.reset_index(inplace=True)
-            # note which genes are over- and underexpressed
-            numrows = frame.shape[0]
-            emptycol = ['blank']*numrows
-            frame['expressionChange'] = emptycol
-            for row in range(numrows):
-                if frame.at[row, 'log2FoldChange'] < 0:
-                    frame.at[row, 'expressionChange'] = 'underexpressed'
-                elif frame.at[row, 'log2FoldChange'] > 0:
-                    frame.at[row, 'expressionChange'] = 'overexpressed'
-
-            DEgenes = DEgenes.append(frame)
-
-        except Exception as e:
-            print(Exception, e)
-            print(file)
-
-    # TODO: should I keep duplicate with most significant padj value regardless of timepoint? Or just 
-    # stick to the first timepoint for consistency? Will I even need to drop duplicates in the final data, 
-    # or will each timepoint be its own thing?
-
-    # check if duplicates are overexpressed at one timepoint and underexpressed at another
-    # print("Duplicates:")
-    # temp = DEgenes[DEgenes.duplicated(subset='geneNames', keep=False)]
-    # grouped = temp.groupby('geneNames')
-
-    # for name,group in grouped:
-    #     print(name)
-    #     print(group)
-
-    # TODO: at least one gene is overexpressed at one timepoint and underexpressed at another. Even for genes with similar trends,
-    # how should I deal with different l2fc or different padj values? Which gene should I keep, if any?
-    # PERHAPS: keep duplicate with most significant padj value 
-        # remove all dups from one df, keep all dups in another frame for processing. Then at the end, merge frames together
-        # write helper function to handle dups df - keep only gene with highest padj value
-    '''
-    Gm22711
-        geneNames   baseMean  log2FoldChange     lfcSE      stat    pvalue      padj    index expressionChange
-    122   Gm22711  17.088073        1.729882  0.597521  2.829907  0.004656  0.032593  13156.0    overexpressed
-    37    Gm22711  17.088073       -1.949715  0.550194 -3.456516  0.000547  0.035195  13156.0   underexpressed
-    '''
-
-    # if a gene is an outlier at multiple time points, only keep the first entry in the dataframe
-    DEgenes.drop_duplicates(subset='geneNames', inplace=True)
+    DEgenes = pd.read_csv(file, sep=' ', header=0, index_col=0)
+    # Genes with low count numbers and outlier samples are prefiltered by DESeq2 and have a padj of "NA"
     DEgenes.reset_index(inplace=True)
+    DEgenes = DEgenes.rename(columns = {'index':'geneNames'})
+
+    # TODO: look up the default p_adj threshold for DESeq2
+
+    # find differentially expressed genes for each comparison
+    # sort by lfc, keep only stuff below lower -logthresh and above upper logthresh
+    DEgenes = DEgenes[(DEgenes['log2FoldChange'] < -logthresh) | (DEgenes['log2FoldChange'] > logthresh)]
+    # sort by padj (lowest to highest), keep only genes with padj values lower than pthresh
+    DEgenes = DEgenes[DEgenes['padj'] < pthresh]
+    DEgenes.reset_index(inplace=True)
+    # note which genes are over- and underexpressed
+    numrows = DEgenes.shape[0]
+    emptycol = ['blank']*numrows
+    DEgenes['expressionChange'] = emptycol
+    for row in range(numrows):
+        if DEgenes.at[row, 'log2FoldChange'] < 0:
+            DEgenes.at[row, 'expressionChange'] = 'underexpressed'
+        elif DEgenes.at[row, 'log2FoldChange'] > 0:
+            DEgenes.at[row, 'expressionChange'] = 'overexpressed'
+
     return DEgenes
--- network_finder.py
+++ network_finder.py
@@ -1,12 +1,37 @@
-import CleanData
-from py2cytoscape import cyrest
-from py2cytoscape.cyrest.base import api
+import CleanData as cda
+import py4cytoscape as p4c
+
+# # Test that cytoscape is connected
+# p4c.cytoscape_ping()
+# print(p4c.cytoscape_version_info())
+
+# import differential expression data and identify differentially expressed genes
+gene_datafile = ''
+data = cda.CleanData(gene_datafile)
+
+print(data.genes())
+
+# list_of_genes = []
+
+# query STRING database to build a network of protein-protein interactions using the differentially expressed genes
+string_cmd = 'string protein query query=Rgs4 cutoff=0.4 species="Mus musculus" limit=150' # % list_of_genes
+p4c.commands_run(string_cmd)
+
+test_table = p4c.get_table_columns('node','display name')
+print(test_table)
+
+p4c.network_views.export_image(resolution=300)
+
+# # exclude low degree nodes, e.g., those with only 0, 1 or 2 connections
+# p4c.create_degree_filter('degree filter', [0,2], 'IS_NOT_BETWEEN')
+
+# # create a subnetwork of the selected set of nodes and all relevant edges
+# p4c.create_subnetwork(subnetwork_name='highly connected nodes')
+
+
 
 # You MUST open cytoscape on your local machine to run network_finder.py
 
-# Set up cytoscape connection
-cytoscape=cyrest.cyclient()
-cytoscape.session.new()
 
 # Cyni Toolbox is the app that contains the arachne algorithm
 # There is also a "NetworkAnalyzer" bundle that exists
